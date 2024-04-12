6454d9bcdc497892808eb7549416c47c8cac580a
ABDULLAHI OMOWUMI IBRAHEEM <71957195+mojicsibro2@users.noreply.github.com>
2024-04-12 17:22:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function On() {
+    document.getElementById('off').style.display = 'flex';
+    document.getElementById('on').style.display = 'none';
+}
+
+
+function Off() {
+    document.getElementById('off').style.display = 'none';
+    document.getElementById('on').style.display = 'flex';
+}
